7e7086d8d584e2fcecf8191072a10cd208b365cc
hariom-tiwari <30381627+hariom-tiwari@users.noreply.github.com>
2017-07-23 10:38:49 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/bee-naruto.jpg') {
+        myImage.setAttribute('src', 'images/bee-naruto2.png');
+    } else {
+        myImage.setAttribute('src', 'images/bee-naruto.jpg');
+    }
+};
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Naruto and Killer Bee, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName =  localStorage.getItem('name');
+    myHeading.textContent = 'Naruto and Killer Bee, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+};
